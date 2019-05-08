315d7c4c5a29973660667bbd0e06605c9ce0defa
Mengqiao <mengqiao.yu@berkeley.edu>
2019-05-07 17:16:14 -0700

    add test for HeteroMixtureHMM model using real data.

--- test_model.py
+++ test_model.py
@@ -1,7 +1,9 @@
 import numpy as np
 from hmmlearn import hmm
-from hmmlearn import base
 import MixtureHMM
+import logging
+from os import listdir
+import pandas as pd
 
 __author__ = "Mengqiao Yu"
 __email__ = "mengqiao.yu@berkeley.edu"
@@ -9,7 +11,7 @@ __email__ = "mengqiao.yu@berkeley.edu"
 
 # generate multiple sequences of sample
 def sampling_mixture(N=1000, num_states = 2, num_timesteps = 12):
-    """test for BasicHMM"""
+    """test for MixtureHMM"""
     model = hmm.MultinomialHMM(n_components=num_states)
     model.startprob_ = np.array([0.4, 0.6])
     model.transmat_ = np.array([[0.7, 0.3],
@@ -34,15 +36,35 @@ def sampling_mixture(N=1000, num_states = 2, num_timesteps = 12):
     # print(seq[10])
     return seq
 
-def sampling_mixture(N=1000, num_states=2, num_timesteps = 12):
-    """test for MixtureHMM"""
-    #TODO: generate sample for mixture HMM
-    raise NotImplementedError
+# test MixtureHMM (have calibrated with benchmark model)
+def train_mixture():
+    print("This section is for training MixtureHMM example")
+    sample = sampling_mixture()
+    MHMM = MixtureHMM.MixtureHMM(num_states=2)
+    MHMM.train(obs_seq=sample, cutoff_value=1e-3, max_iter=500)
 
+# test HeteroMixtureHMM
+def train_hetero():
+    """"We don't need sampling anymore, we directly feed our own dataset and test the model."""
+    data_dir = '/Users/MengqiaoYu/Desktop/Research/WholeTraveler/Data/model/'
+    print("Load the formatted files in dir: %s into model." %data_dir)
+    data_model = []
+    for f in listdir(data_dir):
+        if f.startswith('.'):
+            continue
+        data_ind = pd.read_csv(data_dir + f)
+        data_model.append(data_ind[['used_public', 'used_own', 'move', 'age', 'employ']].values)
 
-# test BasicHMM
-sample = sampling_mixture()
-MHMM = MixtureHMM.BasicHMM(num_states=2)
-MHMM.train(obs_seq=sample, cutoff_value=1e-3, max_iter=500)
+    # full_header =list(data_ind)
+    header = ['used_public', 'used_own', 'move', 'age', 'employ']
+    choices = ['used_public', 'used_own']
+    trans_cov = ['move', 'age', 'employ']
 
-#
+    HHMM = MixtureHMM.HeteroMixtureHMM(num_states=2)
+    HHMM.set_dataframe(samples=data_model,
+                       header=header,
+                       choices=choices,
+                       trans_cov=trans_cov)
+    HHMM.train(cutoff_value=1e-3, max_iter=200)
+
+train_hetero()
